fb44188281f9b956d0275944e294cfa2cda08e99
JCPedroza <nemesis4go10@hotmail.com>
2022-01-10 16:40:17 -0600

    (34:13): implement mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,7 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+  { id: 1, text: 'doctor', day: 'May 5th at 2:30', reminder: true },
+  { id: 2, text: 'meeting', day: 'Jan 2th at 21:30', reminder: true },
+  { id: 3, text: 'shopping', day: 'Jul 23th at 9:30', reminder: true }
+]
